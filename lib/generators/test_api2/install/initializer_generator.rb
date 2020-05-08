dbcefeaf692e5899ca1c009dd78c4b5843fdd5b7
Noah Camargo <noah.camargo@hotmail.com>
2020-05-08 15:27:55 -0300

    Scoped initializer

--- lib/generators/test_api2/install/initializer_generator.rb
+++ lib/generators/test_api2/install/initializer_generator.rb
@@ -1,9 +1,13 @@
 require 'rails/generators/active_record'
 
-class InitializerGenerator < Rails::Generators::Base
-  def create_rake_task
-    rakefile "test_api.rake" do
-      File.open("#{File.expand_path(__dir__)}/../tasks/test_api2.rake").reduce { |a, c| a.to_s + c.to_s }
+module TestApi2
+  module Generators
+    class InstallGenerator < Rails::Generators::Base
+      def create_rake_task
+        rakefile "test_api.rake" do
+          File.open("#{File.expand_path(__dir__)}/../tasks/test_api2.rake").reduce { |a, c| a.to_s + c.to_s }
+        end
+      end
     end
   end
 end
