b2b9c2e2bc4f04b9eddc62e31b7ee07962a15aae
Noah Camargo <noah.camargo@hotmail.com>
2020-05-08 15:23:03 -0300

    Add install command

--- lib/generators/test_api2/install/initializer_generator.rb
+++ lib/generators/test_api2/install/initializer_generator.rb
@@ -3,7 +3,7 @@ require 'rails/generators/active_record'
 class InitializerGenerator < Rails::Generators::Base
   def create_rake_task
     rakefile "test_api.rake" do
-      File.open('../tasks/test_api2.rake').reduce { |a, c| a.to_s + c.to_s }
+      File.open("#{File.expand_path(__dir__)}/../tasks/test_api2.rake").reduce { |a, c| a.to_s + c.to_s }
     end
   end
 end
